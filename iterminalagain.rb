b038404b09a42bbfcb4052195b189b41a5e162ab
Xavier Simó <xsimov@gmail.com>
2015-08-20 12:13:16 +0200

    Refactored display_slides method because it was too long

--- iterminalagain.rb
+++ iterminalagain.rb
@@ -3,16 +3,20 @@ require 'terminfo'
 class Presentation
   def initialize(slides_file)
     @array_of_texts = FileReader.new(slides_file).get_slides_text
+    @half_screen_height = TermInfo.screen_height / 2
+    @half_screen_width  = TermInfo.screen_width  / 2
   end
 
   def display_slides
-    half_screen_height = TermInfo.screen_height / 2
-    half_screen_width  = TermInfo.screen_width  / 2
     @array_of_texts.each do |text|
-      puts "\n" * half_screen_height
-      puts (" " * (half_screen_width - text.length / 2)) + text
-      puts "\n" * (half_screen_height - 1)
+      print_slide(text)
     end
+  end
+
+  def print_slide(text)
+    puts "\n" * @half_screen_height
+    puts (" " * (@half_screen_width - text.length / 2)) + text
+    puts "\n" * (@half_screen_height - 1)
   end
 end
 
