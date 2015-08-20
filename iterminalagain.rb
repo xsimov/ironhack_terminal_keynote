137d15f5f58150790f2ff14c72e457885360acc6
Xavier Simó <xsimov@gmail.com>
2015-08-20 11:53:20 +0200

    Slides showing in the screen from the file, horizontally centered

---
+++ iterminalagain.rb
@@ -0,0 +1,27 @@
+require 'terminfo'
+
+class Presentation
+  def initialize(slides_file)
+    @array_of_texts = FileReader.new(slides_file).get_slides_text
+  end
+
+  def display_slides
+    half_screen_height = TermInfo.screen_height / 2
+    half_screen_width  = TermInfo.screen_width  / 2
+    @array_of_texts.each do |text|
+      puts (" " * (half_screen_width - text.length / 2)) + text
+    end
+  end
+end
+
+class FileReader
+  def initialize(slides_file)
+    @slides_file = slides_file
+  end
+
+  def get_slides_text
+    IO.read(@slides_file).split("----\n").map(&:chomp)
+  end
+end
+
+Presentation.new("terminal_slideshow.txt").display_slides
