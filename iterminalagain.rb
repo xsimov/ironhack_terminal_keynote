1b0b407ed67549e3f4ce74a492fdafeee6ff45b1
Xavier Simó <xsimov@gmail.com>
2015-08-20 12:35:15 +0200

    Added logic to control slides and show them one at a time

--- iterminalagain.rb
+++ iterminalagain.rb
@@ -5,18 +5,34 @@ class Presentation
     @array_of_texts = FileReader.new(slides_file).get_slides_text
     @half_screen_height = TermInfo.screen_height / 2
     @half_screen_width  = TermInfo.screen_width  / 2
+    @current_slide_position = 0
   end
 
   def display_slides
-    @array_of_texts.each do |text|
-      print_slide(text)
-    end
+    print_current_slide
+    get_user_input
   end
 
-  def print_slide(text)
+  private
+  def print_current_slide
+    text = @array_of_texts[@current_slide_position]
     puts "\n" * @half_screen_height
     puts (" " * (@half_screen_width - text.length / 2)) + text
     puts "\n" * (@half_screen_height - 1)
+    print "> "
+  end
+
+  def get_user_input
+    input = gets.chomp
+    case input.downcase
+    when "next"
+      @current_slide_position += 1
+    when "previous"
+      @current_slide_position -= 1
+    when "quit"
+      return
+    end
+    display_slides
   end
 end
 
